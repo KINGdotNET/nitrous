5e25593961c93b18f9f03843bfe3cec4a0bc2ecb
Mendoza, Emil B <emil@king.net>
2021-03-22 23:59:24 -0400

    update config.js

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -107,6 +107,68 @@ const ONLINEBUZZ = {
     ],
 };
 
+const PLOGGING = {
+    APP_NAME: 'Plogging',
+    APP_ICON: 'plogging',
+    APP_ICON_WIDTH: '200px',
+    APP_ICON_HEIGHT: '40px',
+    APP_URL: 'https://plogging.com',
+    APP_DOMAIN: 'Plogging.com',
+    LIQUID_TOKEN: 'Blurt',
+    LIQUID_TOKEN_UPPERCASE: 'BLURT',
+    APP_MAX_TAG: 10,
+    SCOT_TAG: 'plogging',
+    SCOT_DEFAULT_BENEFICIARY_ACCOUNT; 'yehey',
+    SCOT_DEFAULT_BENEFICIARY_PERCENT; 10,
+    GOOGLE_AD_CLIENT; 'ca-pub-5903031199985375',
+    TAG_LIST: fromJSOrdered([
+        'blurt',
+        'hive',
+        'pesos',
+        'swap',
+        'pets',
+        'news',
+        'media',
+        'finance',
+        'travel',
+        'cybersecurity',
+        'games',
+        'myscoop',
+        'domainname',
+        'entertainment',
+        'sports',
+        'machinelearning',
+    ]),
+    INTERLEAVE_PROMOTED: true,
+    PROMOTED_POST_ACCOUNT: 'null',
+    VESTING_TOKEN: 'BLURT POWER',
+    SITE_DESCRIPTION:
+        'Ploggin is a social media platform where everyone gets paid for ' +
+        'creating and curating content. It leverages a robust digital points system, called PESOS, that ' +
+        'supports real value for digital rewards through market price discovery and liquidity',
+    // Revive Ads
+    NO_ADS_STAKE_THRESHOLD: 9999999999,
+    REVIVE_ADS: {},
+    ALLOW_MASTER_PW: false,
+    // Footer to attach to posts. ${POST_URL} is a macro that can be used, will be expanded to the URL of the post.
+    POST_FOOTER:
+        '\n\n---\n\n<center><sub>Posted via [Plogging.com](${POST_URL}) blog to earn BLURT and HIVE tokens.</sub></center>',
+    COMMENT_FOOTER:
+        '\n\n---\n\n<center><sub>Posted via [Plogging.com](${POST_URL}) blog to earn BLURT and HIVE tokens.</sub></center>',
+    SCOT_TAG_FIRST: false,
+    PINNED_POSTS_URL: null,
+    SDC_GTAG_MEASUREMENT_ID: 'UA-145448693-33',
+    DISABLE_STEEM: true,
+    PREFER_HIVE: true,
+    HIVE_ENGINE: true,
+    DISABLE_BLACKLIST: true,
+    CHAT_CONVERSATIONS: [
+        { id: '01EPB6A2PPSW0BQVJ7WDDP568C', name: 'BeeChat Trollbox' },
+    ],
+};
+
+
+
 export const CONFIG_MAP = {
     // testing heroku/local options
     'localhost:8080': STEEMING,
